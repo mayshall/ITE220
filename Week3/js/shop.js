811585b24a6181b90f636dbe4552fd229d48e6e5
DESKTOP-MQLCUB7\Marshall <anatolii.lipatkin@students.stamford.edu>
2017-04-06 20:48:54 +0700

    Basic JS - Shopping Center (V.1)

---
+++ Week3/js/shop.js
@@ -0,0 +1,14 @@
+var customerName = "May";
+var price;
+var quantity;
+var total;
+
+price = 5;
+quantity = 14;
+total = price * quantity;
+
+var customer = document.getElementById("name");
+customer.textContent = customerName;
+
+var customer = document.getElementById("total");
+customer.textContent = total;
